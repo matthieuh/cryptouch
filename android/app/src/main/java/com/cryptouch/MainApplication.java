52d688cc574cef84f8e8d445be0f66165553a86b
Matthieu Hostache <matthieu.hostache@gmail.com>
2018-03-29 00:26:00 +0200

    Fix syntax error in MainApplication.java

--- android/app/src/main/java/com/cryptouch/MainApplication.java
+++ android/app/src/main/java/com/cryptouch/MainApplication.java
@@ -26,9 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactNativeConfigPackage(),
-            new SvgPackage(),
-            new LinearGradientPackage(),
+          new ReactNativeConfigPackage(),
+          new SvgPackage(),
+          new LinearGradientPackage()
       );
     }
 
